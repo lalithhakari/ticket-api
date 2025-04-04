8e215cb89059368506b8a42421c1d1a14bfedf4f
Lalith Hakari <lalith.axlrose@gmail.com>
2025-04-04 17:21:23 +0530

    mowed files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,10 +19,5 @@ class DatabaseSeeder extends Seeder
         Ticket::factory(100)
             ->recycle($users)
             ->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
